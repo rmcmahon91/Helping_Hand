533a8637351c7380738ed9fe2cd95ed543c684f0
Ryan McMahon <ryan.mcmahon4@gmail.com>
2020-07-23 13:53:25 -0400

    new upload

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app)
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
+
 db.sequelize.sync().then(function () {
     app.listen(PORT, function () {
         console.log("App listening on PORT " + PORT);
