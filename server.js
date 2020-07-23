ea7ddc771477f66a38ecf7a5ed27800e99289192
Ryan McMahon <ryan.mcmahon4@gmail.com>
2020-07-23 13:44:18 -0400

    new upload

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require("express");
    
 // Sets up the Express App
 // =============================================================
-var app = express():
+var app = express();
 var PORT = process.env.PORT || 8008;
 
 // Requiring our models for syncing
